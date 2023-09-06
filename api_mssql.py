34fc93d38ea8833931a945f4c27d3d8ad2c80025
RodrigoReyes27 <89705213+RodrigoReyes27@users.noreply.github.com>
2023-09-06 12:16:55 -0600

    Comit de api con login

--- api_mssql.py
+++ api_mssql.py
@@ -2,14 +2,22 @@ from flask import Flask, make_response, request, jsonify
 import json
 import sys
 import mssql_functions as MSSql
+from dotenv import load_dotenv
+import os
+import jwt
+import time
 
+# Cargar variables de entorno
+load_dotenv()
 # Connect to mssql dB from start
 mssql_params = {}
 # mssql_params['DB_HOST'] = '100.80.80.7'
-mssql_params['DB_HOST'] = '10.14.255.66'
-mssql_params['DB_NAME'] = 'Caritas'
-mssql_params['DB_USER'] = 'SA'
-mssql_params['DB_PASSWORD'] = 'Shakira123.'
+mssql_params['DB_HOST'] = os.getenv('DB_HOST')
+mssql_params['DB_NAME'] = os.getenv('DB_NAME')
+mssql_params['DB_USER'] = os.getenv('DB_USER')
+mssql_params['DB_PASSWORD'] = os.getenv('DB_PASSWORD')
+ACCESS_TOKEN_KEY = os.getenv('ACCESS_TOKEN_KEY')
+REFRESH_TOKEN_KEY = os.getenv('REFRESH_TOKEN_KEY')
 
 try:
     MSSql.cnx = MSSql.mssql_connect(mssql_params)
@@ -19,19 +27,83 @@ except Exception as e:
 
 app = Flask(__name__)
 
+
+# MIddlweware
+@app.before_request
+def verify_jwt():
+    # No verificar token de acceso si el endpoint esta excluido
+    verify_jwt_middleware = True
+    if request.endpoint in app.view_functions:
+        view_func = app.view_functions[request.endpoint]
+        verify_jwt_middleware = not hasattr(view_func, '_exclude_middleware')
+    # Si el endpoint esta excluido, no verificar token de acceso
+    if not verify_jwt_middleware: return
+
+    
+    global ACCESS_TOKEN_KEY
+    auth_header = request.headers.get('Authorization')
+    
+    if not auth_header:
+        return make_response(jsonify({'error': 'No se proporciono token de acceso'}), 401)
+    
+    # Extraer token de acceso sin el prefijo 'Bearer '
+    access_token = auth_header.replace('Bearer ', '')
+
+    try:
+        payload = jwt.decode(access_token, ACCESS_TOKEN_KEY, algorithms=['HS256'])
+
+        # Verificar si el token esta caducado
+        if 'exp' in payload and payload['exp'] < int(time.time()):
+            return {'error': 'Token de acceso caducado'}, 401
+        request.userJWT = payload
+    
+    except jwt.ExpiredSignatureError:
+        return {'error': 'Token de acceso caducado'}, 401
+    except jwt.DecodeError:
+        return {'error': 'Token de acceso invalido'}, 401
+    except Exception as e:
+        raise TypeError("Error al verificar token de acceso: %s" % e)
+
+
+def exclude_middleware(func):
+    func._exclude_middleware = True
+    return func
+
+
 @app.route("/hello")
 def hello():
     return "Las langostas rojas de Xalapa te saludan de vuelta!\n"
 
+
 @app.route("/sign-in", methods=['POST'])
-def sign_id():
+@exclude_middleware
+def sign_in():
     req = request.json
     username = {'username': req['username']}
     password = {'password': req['password']}
 
     response = MSSql.sign_in(username, password)
 
-    return make_response(jsonify(response))
+    return make_response(response)
+
+
+@app.route("/get-recolector-tickets", methods=['GET'])
+def get_recolector_tickets():
+    userId = request.args.get('userId')
+
+    response = MSSql.get_recolector_tickets(userId, request.userJWT)
+
+    return make_response(response)
+
+
+@app.route("/get-ticket", methods=['GET'])
+def get_recolector_tickets():
+    ticketId = request.args.get('ticketId')
+
+    response = MSSql.get_ticket_information(userId, request.userJWT)
+
+    return make_response(response)
+
 
 if __name__ == '__main__':
     print ("Running API...")
--- mssql_functions.py
+++ mssql_functions.py
@@ -1,11 +1,17 @@
 import pymssql
 import bcrypt
 import jwt
+import datetime
+import time
+from dotenv import load_dotenv
+import os
 
+# Cargar variables de entorno
+load_dotenv()
 cnx = None
 mssql_params = {}
-ACCESS_TOKEN_KEY = "637c9e8eff1fda4ce059ea3a4ff307bfb3015aa598171c6e19bfaf02b66e8c6c"
-REFRESH_TOKEN_KEY = "86ec9822ce887f3efe08d207c1f9ddc2d22573180a124347fe7247ed1d27e477"
+ACCESS_TOKEN_KEY = os.getenv('ACCESS_TOKEN_KEY')
+REFRESH_TOKEN_KEY = os.getenv('REFRESH_TOKEN_KEY')
 
 def mssql_connect(sql_creds):
     import pymssql
@@ -17,12 +23,38 @@ def mssql_connect(sql_creds):
     return cnx
 
 
+def calculate_expiration():
+    return datetime.datetime.utcnow() + datetime.timedelta(days=1)
+
+
+def post_refresh_token(token, userId):
+    global cnx, mssql_params
+    query = 'UPDATE USUARIOS SET refreshtoken = %s WHERE id_usuario = %s;'
+
+    try:
+        cursor = cnx.cursor(as_dict=True)
+        cursor.execute(query, (token, userId))
+        cnx.commit()  # Commit the changes to the database
+        cursor.close()
+    except pymssql.InterfaceError as ie:
+        print('Error executing SQL query:', ie)
+        # Reconnect to the database
+        cnx = mssql_connect(mssql_params)
+        cursor = cnx.cursor(as_dict=True)
+        cursor.execute(query, (token,))
+        cnx.commit()  # Commit the changes to the database
+        cursor.close()
+    except Exception as e:
+        print("Error al iniciar sesion:", e)
+        raise TypeError("Error al iniciar sesion: %s" % e)
+
+
 def sign_in(username, password):
     global cnx, mssql_params, ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY
     username_value = username['username']
     password_value = password['password']
     # Parametrized query (allows only strings)
-    query = 'SELECT userid, username, name, lastname, role, password FROM [user] WHERE username = %s;'
+    query = 'SELECT id_usuario, username, role, password FROM USUARIOS WHERE username = %s;'
 
     try:
         try:
@@ -30,7 +62,6 @@ def sign_in(username, password):
             # Execute query using username as parameter
             cursor.execute(query, (username_value))
         except pymssql._pymssql.InterfaceError:
-            pass
             print("La langosta se esta conectando...")
             cnx = mssql_connect(mssql_params)
             cursor = cnx.cursor(as_dict=True)
@@ -41,31 +72,60 @@ def sign_in(username, password):
         
         cursor.close()
 
-        if bcrypt.checkpw(password['password'].encode('utf-8'),user_password):      
-            resultado = {"userId": result[0]['userid'], 
-                         "userName": result[0]['username'], 
-                         "name": result[0]['name'], 
-                         "lastName": result[0]['lastname'], 
-                         "role": result[0]['role'],
-                         }
+        if bcrypt.checkpw(password_value.encode('utf-8'),user_password):      
+            resultado = {
+                            "userId": result[0]['id_usuario'], 
+                            "userName": result[0]['username'], 
+                            # "name": result[0]['name'], 
+                            # "lastName": result[0]['lastname'], 
+                            "role": result[0]['role'],
+                            "exp": calculate_expiration()
+                        }
+            
             accessToken = jwt.encode(resultado, ACCESS_TOKEN_KEY, algorithm='HS256')
             refreshToken = jwt.encode(resultado, REFRESH_TOKEN_KEY, algorithm='HS256')
             resultado['accessToken'] = accessToken
             resultado['refreshToken'] = refreshToken
 
+            post_refresh_token(refreshToken, resultado['userId'])
             return resultado
+
         else:
-            return {'error': 'Usuario o contraseña incorrectos'}
+            return {'error': 'Usuario o contraseña incorrectos'}, 401
         
     except Exception as e:
-        raise TypeError("Error al iniciar sesion: %s" % e)
+        return {'error': e}, 401
+        # raise TypeError("Error al iniciar sesion: %s" % e)
+
+
+def get_recolector_tickets(user_id, jwt_payload):
+    global cnx, mssql_params, ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY
+    # query = """
+    #         SELECT * t.ID_NUM_PAGO, t.IMPORTE,
+    #         """
+
+    try:
+        if 'userId' in jwt_payload and jwt_payload['userId'] != int(user_id):
+            return {'error': 'Acceso no autorizado'}, 401
+
+        return [
+            {'idTicket': 1234, 'importe': 100.00, 'direccion': 'Calle 1', 'nombre': 'Juan'},
+            {'idTicket': 1234, 'importe': 100.00, 'direccion': 'Calle 1', 'nombre': 'Juan'},
+            {'idTicket': 1234, 'importe': 100.00, 'direccion': 'Calle 1', 'nombre': 'Juan'},
+            ], 200
+
+    except Exception as e:
+        return {'error': 'Ocurrio un error al obtener tickets de recolector'}
+        # raise TypeError(f"Error al obtener tickets de recolector: {userId}. Error: {e}")
+    
 
 
 if __name__ == '__main__':
     import json
     mssql_params = {}
-    mssql_params['DB_HOST'] = '10.14.255.66'
-    mssql_params['DB_NAME'] = 'Caritas'
-    mssql_params['DB_USER'] = 'SA'
-    mssql_params['DB_PASSWORD'] = 'Shakira123.'
+    mssql_params['DB_HOST'] = os.getenv('DB_HOST')
+    mssql_params['DB_NAME'] = os.getenv('DB_NAME')
+    mssql_params['DB_USER'] = os.getenv('DB_USER')
+    mssql_params['DB_PASSWORD'] = os.getenv('DB_PASSWORD')
+
     cnx = mssql_connect(mssql_params)
